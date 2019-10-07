84250666b3d32ebd05c4614309c1ae0613f8a2d3
Isidro Fernando Gonzalez Gomez <Chilito395@gmail.com>
2019-10-07 07:32:30 -0500

    Se creo el metodo de añadir peliculas y se elimino sumar restar multiplicar y dividir, en este metodo se uso una lista de listas de string.

--- RMI/Cliente.java
+++ RMI/Cliente.java
@@ -3,6 +3,7 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Cliente {
     private static final String IP = "127.0.0.1"; // Puedes cambiar a localhost
@@ -13,8 +14,9 @@ public class Cliente {
         Interfaz interfaz = (Interfaz) registry.lookup("Calculadora"); //Buscar en el registro...
         Scanner sc = new Scanner(System.in);
         int eleccion;
-        float numero1, numero2, resultado = 0;
-        String menu = "\n\n------------------\n\n[-1] => Salir\n[0] => Sumar\n[1] => Restar\n[2] => Multiplicar\n[3] => Dividir\nElige: ";
+        String[] AtributosPelicula = new String[5];
+        ArrayList<ArrayList<String>> resultado = new ArrayList<ArrayList<String>>();
+        String menu = "\n\n------------------\n\n[-1] => Salir\n[0] => Agregar\nElige: ";
         do {
             System.out.println(menu);
 
@@ -23,41 +25,60 @@ public class Cliente {
             } catch (NumberFormatException e) {
                 eleccion = -1;
             }
+            switch (eleccion) {
+                case 0:
+                    System.out.println("Ingresa Nombre: ");
+                    try{
+                        AtributosPelicula[0] = sc.nextLine();
+                    }catch(NumberFormatException e){
+                        AtributosPelicula[0] = "";
+                    }
 
-            if(eleccion != -1){
+                    System.out.println("Ingresa Clasificacion: ");
+                    try{
+                        AtributosPelicula[1] = sc.nextLine();
+                    }catch(NumberFormatException e){
+                        AtributosPelicula[1] = "";
+                    }
 
-                System.out.println("Ingresa el numero 1: ");
-                try{
-                    numero1 = Float.parseFloat(sc.nextLine());
-                }catch(NumberFormatException e){
-                    numero1 = 0;
-                }
+                    System.out.println("Ingresa Horario: ");
+                    try{
+                        AtributosPelicula[2] = sc.nextLine();
+                    }catch(NumberFormatException e){
+                        AtributosPelicula[2] = "";
+                    }
 
-                System.out.println("Ingresa el numero 2: ");
-                try{
-                    numero2 = Float.parseFloat(sc.nextLine());
-                }catch(NumberFormatException e){
-                    numero2 = 0;
-                }
-                switch (eleccion) {
-                    case 0:
-                        resultado = interfaz.sumar(numero1, numero2);
-                        break;
-                    case 1:
-                        resultado = interfaz.restar(numero1, numero2);
-                        break;
-                    case 2:
-                        resultado = interfaz.multiplicar(numero1, numero2);
-                        break;
-                    case 3:
-                        resultado = interfaz.dividir(numero1, numero2);
-                        break;
-                }
+                    System.out.println("Ingresa Sala: ");
+                    try{
+                        AtributosPelicula[3] = sc.nextLine();
+                    }catch(NumberFormatException e){
+                        AtributosPelicula[3] = "";
+                    }
 
-                System.out.println("Resultado => " + String.valueOf(resultado));
-                System.out.println("Presiona ENTER para continuar");
-                sc.nextLine();
+                    System.out.println("Ingresa Genero: ");
+                    try{
+                        AtributosPelicula[4] = sc.nextLine();
+                    }catch(NumberFormatException e){
+                        AtributosPelicula[4] = "";
+                    }
+                    resultado.add(interfaz.AddPelicula(AtributosPelicula[0], AtributosPelicula[1], AtributosPelicula[2], AtributosPelicula[3] ,AtributosPelicula[4]));
+                    System.out.println("\n PELICULA AGREGADA \n");
+
+                    System.out.println("Peliculas: \n");
+                    for (int i = 0; i < resultado.size(); i++){
+                        System.out.println(resultado.get(i) + "\n");
+                    }
+
+                    break;
+
+                case 2: //AQUI VA TU PARTE JJ
+
+                case 3: //AQUI VA TU PARTE LEWISAURIO
             }
+
+            System.out.println("Presiona ENTER para continuar");
+            sc.nextLine();
+
         } while (eleccion != -1);
     }
 }
--- RMI/Servidor.java
+++ RMI/Servidor.java
@@ -1,11 +1,11 @@
+ import java.rmi.AlreadyBoundException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
 
-
-        import java.rmi.AlreadyBoundException;
-        import java.rmi.Remote;
-        import java.rmi.RemoteException;
-        import java.rmi.registry.LocateRegistry;
-        import java.rmi.registry.Registry;
-        import java.rmi.server.UnicastRemoteObject;
 public class Servidor {
     private static final int PUERTO = 1100; //Si cambias aquí el puerto, recuerda cambiarlo en el cliente
 
@@ -15,32 +15,18 @@ public class Servidor {
                 Sobrescribir opcionalmente los métodos que escribimos en la interfaz
             */
             @Override
-            public float sumar(float numero1, float numero2) throws RemoteException {
-                return numero1 + numero2;
+            public ArrayList<String> AddPelicula(String nombre, String clasificacion, String duracion, String Sala, String genero) throws RemoteException {
+                ArrayList<String> pelicula = new ArrayList<String>();
+                pelicula.add(nombre);
+                pelicula.add(clasificacion);
+                pelicula.add(duracion);
+                pelicula.add(Sala);
+                pelicula.add(genero);
+                return pelicula;
             }
 
             ;
 
-            @Override
-            public float restar(float numero1, float numero2) throws RemoteException {
-                return numero1 - numero2;
-            }
-
-            ;
-
-            @Override
-            public float multiplicar(float numero1, float numero2) throws RemoteException {
-                return numero1 * numero2;
-            }
-
-            ;
-
-            @Override
-            public float dividir(float numero1, float numero2) throws RemoteException {
-                return numero1 / numero2;
-            }
-
-            ;
         }, 0);
         Registry registry = LocateRegistry.createRegistry(PUERTO);
         System.out.println("Servidor escuchando en el puerto " + String.valueOf(PUERTO));
